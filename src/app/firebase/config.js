c4597becf206439125eee0b30b2177ca9959ca08
RN Connected <132627934+rnconnected@users.noreply.github.com>
2024-02-24 11:54:21 +0100

    Update config.js

--- src/app/firebase/config.js
+++ src/app/firebase/config.js
@@ -26,24 +26,13 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
-// const firebaseConfig = {
-//   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-//   authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-//   projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-//   storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-//   messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-//   appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-// };
-
 const firebaseConfig = {
-  apiKey: "AIzaSyCQDmrezS6tE5Pn9ocC_R5CZ7PtJSog4EA",
-  authDomain: "certracker.firebaseapp.com",
-  databaseURL: "https://certracker-default-rtdb.firebaseio.com",
-  projectId: "certracker",
-  storageBucket: "certracker.appspot.com",
-  messagingSenderId: "912576608221",
-  appId: "1:912576608221:web:a4e4a888a1526157f9a834",
-  measurementId: "G-0HCDHT9EVD"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
 
 let app;

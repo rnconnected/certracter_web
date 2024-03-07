7920b88882c73cfd3714ecb94baf809e718ebd9f
Chimaobim Dike Gabriel <89916820+moses-obi@users.noreply.github.com>
2024-03-07 11:15:37 +0100

    update

--- src/app/(dashboard)/(viewDoc)/viewCert/page.jsx
+++ src/app/(dashboard)/(viewDoc)/viewCert/page.jsx
@@ -1,12 +1,47 @@
 "use client";
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Header from "@/components/dashboard/header";
 import Link from "next/link";
 import { Icon } from "@iconify/react";
 import "@/styles/dashboard/viewCert.css";
 import Image from "next/image";
+import { useRouter } from "next/router";
+import { useAuth } from "@/app/hooks/useAuth";
+import { getDoc, doc } from "firebase/firestore";
+import { firestore } from "@/app/firebase/config";
 
 const ViewCert = () => {
+  const [documentData, setDocumentData] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const router = useRouter();
+  const { collection, id } = router.query;
+
+  useEffect(() => {
+    const fetchDocumentData = async () => {
+      try {
+        if (!collection || !id) return;
+
+        setLoading(true);
+
+        const docRef = doc(firestore, collection, id);
+        const docSnapshot = await getDoc(docRef);
+
+        if (docSnapshot.exists()) {
+          setDocumentData({ id: docSnapshot.id, ...docSnapshot.data() });
+        } else {
+          console.error("Document not found.");
+        }
+
+        setLoading(false);
+      } catch (error) {
+        console.error("Error fetching document data:", error);
+        setLoading(false);
+      }
+    };
+
+    fetchDocumentData();
+  }, [collection, id]);
+
   return (
     <>
       <Header />
